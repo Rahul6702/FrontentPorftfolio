f9626b9ede9e038aeca28696754c180a7ad42c16
RAHUL KUMAR <123005909+Rahul6702@users.noreply.github.com>
2023-12-01 21:10:20 +0530

    commite successfully

---
+++ main.js
@@ -0,0 +1,10 @@
+
+// hamburger toggle close nd open
+
+const header = document.querySelector("header");
+const hamburgerBtn = document.querySelector("#hamburger-btn");
+const closeMenuBtn = document.querySelector("#close-menu-btn");
+
+hamburgerBtn.addEventListener("click", () => header.classList.toggle("show-mobile-menu"));
+
+closeMenuBtn.addEventListener("click", () => hamburgerBtn.click());
